4fd1e4fb3f0b4f9805dc4a44f4d3bd84655615fc
Biplab Adhikary <109418981+badhi2020@users.noreply.github.com>
2022-07-16 19:40:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,6 @@ streamlit.code("a=123, b=234, c=a+b")
 
 
 
-import panda
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
