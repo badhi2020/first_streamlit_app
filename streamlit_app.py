4dbffeaccd608cd740404a8b4f0a59c5d18fa33d
Biplab Adhikary <109418981+badhi2020@users.noreply.github.com>
2022-07-16 19:12:29 +0530

    Update streamlit_app.py
    making some lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,10 @@ import streamlit
 
 streamlit.title("This is the Title")
 
+streamlit.header("This is the Header")
+
+streamlit.Subheader("This is the Subheader")
+
+streamlit.caption("This is the Caption")
+
+streamlit.codeblock("print('this is awesome')")
