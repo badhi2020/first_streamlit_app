b3fb6ba840b61d0c9670ebb1e67637c972ff623c
Biplab Adhikary <109418981+badhi2020@users.noreply.github.com>
2022-07-16 19:14:16 +0530

    Update streamlit_app.py
    Fixing codeblock

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,4 @@ streamlit.subheader("This is the Subheader")
 
 streamlit.caption("This is the Caption")
 
-streamlit.codeblock("print('this is awesome')")
+streamlit.codeblock("This is the Codeblock")
